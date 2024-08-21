912f30ebc71028302692e4177e7e365f880c1257
andrastoth021 <andrastoth021@gmail.com>
2024-08-21 10:10:42 +0200

    implement fetching the heroes and rendering them

--- diligent-academy-assessments/frontend/react-challenge/src/heroes-list.tsx
+++ diligent-academy-assessments/frontend/react-challenge/src/heroes-list.tsx
@@ -1,7 +1,46 @@
+import {
+  useEffect,
+  useState
+} from "react";
+import { callApi } from "./call-api";
+
+interface Hero {
+  id: number,
+  name: string,
+  available: boolean
+}
+
 function HeroesList() {
+  const [heroes, setHeroes] = useState<Hero[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    callApi("heroes")
+      .then(data => {
+        // @ts-ignore
+        setHeroes(data);
+        setIsLoading(false);
+      })
+      .catch(err => {
+        setError(err);
+        setIsLoading(false);
+      })
+
+  }, []);
+
+  if ( isLoading ) return <h2>Loading...</h2>;
+  if ( error ) return <h2>Error: {error}</h2>;
+
   return (
     <>
       <h2>Heroes</h2>
+      { heroes && heroes.map((hero: Hero) => {
+        return (
+          <div key={hero.id}>{hero.id}. {hero.name}: {hero.available ? "Available" : "NOT available"}</div>
+        )
+      }
+      )}
     </>
   );
 }
